a44b11508c5f88701fe4c309663fc5a9b3104840
Bharath Ravi <bharathravi1@gmail.com>
2014-10-11 20:40:21 -0700

    Adding jinja

--- helloworld.py
+++ helloworld.py
@@ -14,6 +14,10 @@ MAIN_PAGE_HTML = """\
 """
 
   
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
   def GetUserOrRedirect(self, uri):
